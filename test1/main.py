5119fcd21f6eb7c2b09b45c8558b48159225057a
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2021-08-25 21:28:45 -0500

    Add files via upload

---
+++ test1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 08:22:18 2021
+
+@author: rotoapanta
+"""
+
+import module.py
+
+
---
+++ test1/module.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 09:21:33 2021
+
+@author: rotoapanta
+"""
+
+print("Genial!! esto es module")
